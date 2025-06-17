74b9acb2aba1890b779e8fd1fd49454f3f4c8231
Ravik11 <ravi.kanyal11@gmail.com>
2025-06-17 17:58:57 +0530

    Update streamlit_app.py
    select(col('FRUIT_NAME'), col('SEARCH_ON'))

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ st.write("The Name on your smoothie :cup_with_straw: will be -", name_on_order)
 
 
 
-my_dataframe = session.table("smoothies.public.FRUIT_OPTIONS").select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.FRUIT_OPTIONS").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
